0458425a789788fc9729652f76eb38d218220182
Leo <datum19691969@gmail.com>
2023-10-16 18:04:41 -0400

    wip

--- src/features/auth/AuthPopUp.tsx
+++ src/features/auth/AuthPopUp.tsx
@@ -1,10 +1,11 @@
 import { Auth0ContextInterface } from "@auth0/auth0-react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const domain = "dev-1tkiivqacmubkas5.us.auth0.com";
 export const AuthPopUp: React.FC<{ authObject: Auth0ContextInterface, show: any, children: any }> =
     ({ authObject, show, children }) => {
         const [showModal, setShowModal] = useState(false);
+        const childWindowRef = useRef<Window>();
 
         const handleShowModal = () => {
             setShowModal(true);
@@ -14,8 +15,21 @@ export const AuthPopUp: React.FC<{ authObject: Auth0ContextInterface, show: any,
             setShowModal(false);
         };
         const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+        const openChildWindow = () => {
+            const childWindow = window.open('', '_blank', 'height=400,width=400');
+            if (childWindow) {
+              childWindowRef.current = childWindow;
+            }
+          };
+        
+          const closeChildWindow = () => {
+            if (childWindowRef.current) {
+              childWindowRef.current.close();
+            }
+          };
         useEffect(() => {
-            const authWindow = window.open('', '_blank', 'width=500,height=600');
+            //const authWindow = window.open('', '_blank', 'width=500,height=600');
+            openChildWindow()  //puts window into Ref
 
             const Prom = new Promise((resolve, reject) => {
                 let accessTokenAuth0: string
@@ -27,7 +41,7 @@ export const AuthPopUp: React.FC<{ authObject: Auth0ContextInterface, show: any,
                     }
                 }
                 authObject.getAccessTokenWithPopup(differentAudienceOptions,
-                    { timeoutInSeconds: 30, popup: authWindow }
+                    { timeoutInSeconds: 30, popup: childWindowRef.current }
                 ).then((accessToken: any) => {
                     accessTokenAuth0 = accessToken as string
                     console.log('auth0 AccessToken', accessToken);
@@ -48,8 +62,26 @@ export const AuthPopUp: React.FC<{ authObject: Auth0ContextInterface, show: any,
                     reject(error + '(while getting Auth0 API JWT)');
                 });
             })
+            if (childWindowRef.current)
+            {
+                childWindowRef.current.addEventListener('blur', function(event) {
+                // Perform actions when the window loses focus
+                console.log('childWindowRef.current lost focus');
+                // Add your custom logic here
+              });
+            }
+            if (childWindowRef.current) {
+                window.addEventListener('beforeunload', closeChildWindow);
+            }
+            return () => {
+                window.removeEventListener('beforeunload', closeChildWindow);
+            };
         }, []);
-
+        const handleKeepOnTop = () => {
+            if (childWindowRef.current) {
+              childWindowRef.current.focus();
+            }
+          };
         return (<div className={showHideClassName}>
             <section className="modal-main">
                 {children}
