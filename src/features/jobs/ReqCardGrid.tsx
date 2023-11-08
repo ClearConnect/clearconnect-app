97c2006b0e4739154ddb71e95515e3053d83f6ad
Leo <datum19691969@gmail.com>
2023-11-08 12:09:34 -0500

    wip

--- src/features/jobs/ReqCardGrid.tsx
+++ src/features/jobs/ReqCardGrid.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Box,  /* CardMedia, */ Grid, Typography } from '@mui/material';
+import { Box, FormControlLabel, FormGroup,  /* CardMedia, */ Grid, Switch, Typography } from '@mui/material';
 //import MyCardImage from '../../logo.svg';
 import ReqCard from './ReqCard';
 import { useGetJobsForContactQuery, IdProp, useGetLovQuery } from '../api/ClearConnectApiSlice'
 import { ProgressBar } from '../../theme/Theme';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
+import Chip from '@mui/material/Chip';
+import { Stack } from '@mui/system';
 //import { ExpandableBox } from '../../app/transitions';
 
 /*  export interface IdProp {
@@ -21,7 +23,14 @@ export const ReqCardGrid: React.FC<IdProp> = (cntId) => {
     error,
     //refetch
   } = useGetJobsForContactQuery(cntId.Id)
-
+  const {
+    data: consultantReqInterests,
+    isLoading: isLoadingChip,
+    isSuccess: isSuccessChip,
+    isError: isErrorChip,
+    error: errorChip,
+    //refetch
+  } = useGetLovQuery()
   const {
     isLoading: isLoadingLov,
     isSuccess: isSuccessLov,
@@ -29,7 +38,18 @@ export const ReqCardGrid: React.FC<IdProp> = (cntId) => {
     error: errorLov
     //refetch
   } = useGetLovQuery()
+  interface ChipData {
+    key: number;
+    label: string;
+  }
+  
+  const [chipData, setChipData] = React.useState<readonly ChipData[]>(
+    consultantReqInterests.map( (cri: any) => { return {key: cri.id, label: cri.cnsintDescription}})
+  );
 
+  const handleDelete = (chipToDelete: ChipData ) => () => {
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+  };
   //const Result =  useGetJobsForContactQuery(2037)
   let boxContent: React.ReactElement | null = null
   let gridItems = null
@@ -40,23 +60,34 @@ export const ReqCardGrid: React.FC<IdProp> = (cntId) => {
     const MyFetchBaseQueryError = error as FetchBaseQueryError;
     boxContent = <ProgressBar message={`Sorry, ${MyFetchBaseQueryError.status.toString()} has occured in ReqCardGrid.`} />
     //boxContent = <>Sorry, an error occured: {MyFetchBaseQueryError.status} {MyFetchBaseQueryError.data}</>
-  }  else if(isSuccess /* && isSuccessLov */){
+  } else if (isSuccess /* && isSuccessLov */) {
     gridItems = Reqs?.map(req => {
       //const c = req['jrId']
       return (<Grid item key={req['jrId']} xs={12} sm={6} md={4} lg={3} xl={2}>
-        <ReqCard ReqCardData={req}  />
+        <ReqCard ReqCardData={req} />
       </Grid>)
     })
-  } else {  boxContent = <div> ????????????????</div>}
+  } else { boxContent = <div> ????????????????</div> }
   return (
     (boxContent ? <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       <Typography variant="h4" component="h1">
         {boxContent}
       </Typography>
     </Box> :
-      <Grid container spacing={2}>
-        {gridItems}
-      </Grid>
+      <>
+
+        <Stack direction={"row"} spacing={1}>
+           {chipData.map( (kuku: ChipData) => <Chip
+              //icon={icon}
+              label={kuku.label}
+              onDelete={ handleDelete(kuku)}
+            />)}
+          </Stack>
+
+        <Grid container spacing={2}>
+          {gridItems}
+        </Grid>
+      </>
     )
   )
 }
